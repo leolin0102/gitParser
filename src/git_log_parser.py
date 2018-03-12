c2bbe90c75e0422c7d734da5b5a80f87a288350f
linlin.leo <linlin.leo@bytedance.com>
2018-03-13 01:14:53 +0800

    读取日志行
    Change-Id: Ie36e8c68e23099db1e962a52fc07e5b93bc6500c

--- src/git_log_parser.py
+++ src/git_log_parser.py
@@ -1,17 +1,32 @@
 import git
-
+import re
 
 def git_log():
     g = git.Git('/Users/leo/dev/Larks/major/ios-client')
     return g.log('--stat', '--since=2018-03-11')
 
+
 def parse_log():
+    items = []
     logs = git_log().splitlines(True)
     for l in logs:
-        print(l)
+        result = parse_commit(l, None)
+        if result is not None:
+            pass
+        else:
+            pass
 
 
 def add_commit_id(change_log_item, commit_id):
+    pass
+
+
+def parse_commit(line, detailParser):
+    result = re.match("commit .*", line)
+    return result
+
+
+def parse_commit_file(line):
     pass
 
 
